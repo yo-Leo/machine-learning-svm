faa3ebc68109521739c9d0e559d1e654853888aa
Leonardo Ruas <182309207+yo-Leo@users.noreply.github.com>
2026-09-25 10:02:41 -0300

    doc: comentario né

--- src/avaliacao.py
+++ src/avaliacao.py
@@ -7,15 +7,15 @@ from sklearn.metrics import (
 
 def avaliar_modelo(modelo, scaler, X_teste, y_teste):
 
-    X_teste_escalado = scaler.transform(X_teste)
+    X_teste_escalado = scaler.transform(X_teste) # Utiliza a mesma scala definida no treino para padronizar os dados
 
-    previsoes = modelo.predict(X_teste_escalado)
+    previsoes = modelo.predict(X_teste_escalado) # Tenta realizar a previsão dos dados de teste
 
-    acuracia = accuracy_score(y_teste, previsoes)
+    acuracia = accuracy_score(y_teste, previsoes) # Mede número de acertos
 
     labels = sorted(y_teste.unique())
 
-    matriz = confusion_matrix(y_teste, previsoes, labels=labels)
+    matriz = confusion_matrix(y_teste, previsoes, labels=labels) # Mede resultado da matriz de confusão
 
     relatorio = classification_report(
         y_teste,
